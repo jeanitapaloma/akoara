7f6647ced762408efba87e5aa89b602d41423c0d
Lucifer Morningstar <star808fer@gmail.com>
2020-08-07 16:24:54 +0800

    wala lang

---
+++ index.js
@@ -0,0 +1,16 @@
+var app = require('express')();
+var express = require('express');
+var http = require('http').createServer(app);
+var port = process.env.PORT || 3000;
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname + '/');
+});
+app.use('/Images', express.static(__dirname + '/Images'));
+
+// app.listen(process.env.PORT || 3000, function(){
+//     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+
+app.listen(port, function(){
+    console.log('listening on port '+port);
+});
